fd3b11c7a2a58ca1483787175f81ff153e31b403
cmandale <45108658+cmandale@users.noreply.github.com>
2024-03-07 20:21:15 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,13 @@
 <?php
+
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
 $targetDirectory = "uploads/";
 $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
